f4a5eb6e1ef6e5927ce0742e06c3f62380e3d258
jinjurajan <jinju.rajan@confluxtechnologies.com>
2016-11-04 19:03:34 +0530

    #2246: Removing client identifier status option hardcodided values

--- app/scripts/controllers/client/ClientIdentifierController.js
+++ app/scripts/controllers/client/ClientIdentifierController.js
@@ -4,16 +4,11 @@
             scope.clientId = routeParams.clientId;
             scope.formData = {};
             scope.documenttypes = [];
-            scope.statusTypes = [{
-                id: 1,
-                label: 'Active'
-            }, {
-                id: 2,
-                label: 'Inactive',
-            }];
+            scope.statusTypes =[];
             resourceFactory.clientIdenfierTemplateResource.get({clientId: routeParams.clientId}, function (data) {
                 scope.documenttypes = data.allowedDocumentTypes;
                 scope.formData.documentTypeId = data.allowedDocumentTypes[0].id;
+                scope.statusTypes = data.clientIdentifierStatusOptions;
             });
 
             scope.validateRsaIdnumber=function()
