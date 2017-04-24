57f1baefb5b2bbc44fbaf0d66df1483a2ba68794
himadri <himadri.mitra@confluxtechnologies.com>
2017-04-24 18:41:17 +0530

    4341-adding all properties in default config file

--- app/scripts/services/UIConfigService.js
+++ app/scripts/services/UIConfigService.js
@@ -4,64 +4,65 @@
         UIConfigService: function ($q,$http) {
 
             this.init = function (scope, tenantIdentifier) {
-                $http.get('scripts/config/' + tenantIdentifier + '_UiConfig.json').success(function (tenantdata) {
-                    checkAndSetData(scope,tenantdata);
-                }).error(function (tenantdata) {
+                $http.get('scripts/config/' + tenantIdentifier + '_UiConfig.json').success(function (tenantSpecificData) {
+                    checkAndSetData(tenantSpecificData);
+                }).error(function (tenantSpecificData) {
                     assignDefaultData();
                 }).catch(function (e) {
                     assignDefaultData();
                 });
-            };
 
-            assignDefaultData = function () {
-                $http.get('scripts/config/default_UiConfig.json').success(function (data) {
-                    if (data != 'undefined' && data != null && data != '') {
-                        if (data.enableUIDisplayConfiguration != null && data.enableUIDisplayConfiguration == true) {
-                            scope.response = data;
-                            scope.responseDefaultGisData = data;
+
+                assignDefaultData = function () {
+                    $http.get('scripts/config/default_UiConfig.json').success(function (data) {
+                        if (data != 'undefined' && data != null && data != '') {
+                            if (data.enableUIDisplayConfiguration != null && data.enableUIDisplayConfiguration == true) {
+                                scope.response = data;
+                                scope.responseDefaultGisData = data;
+                            }
+                        }
+                    }).error(function (data) {
+                        console.log("Configuration file not found");
+                    }).catch(function (e) {
+                        console.log("Configuration file not found");
+                    });
+                };
+
+                checkAndSetData = function (tenantSpecificData) {
+                    $http.get('scripts/config/default_UiConfig.json').success(function (defaultData) {
+                        var result = {};
+                        result = diff(defaultData, tenantSpecificData);
+                        if (result.enableUIDisplayConfiguration != null && result.enableUIDisplayConfiguration == true) {
+                            scope.response = result;
+                            scope.responseDefaultGisData = result;
+                        }
+                    }).error(function (tenantSpecificData) {
+                        console.log("Configuration file not found");
+                    }).catch(function (e) {
+                        console.log("Configuration file not found");
+                    });
+                };
+
+                var diff = function (defaultData, tenantSpecificData) {
+                    var result = defaultData;
+                    for (var label in tenantSpecificData) {
+                        if (typeof tenantSpecificData[label] == 'object' && typeof defaultData[label] == 'object') {
+                            result[label] = diff(defaultData[label], tenantSpecificData[label]);
+
+                        } else {
+                            result[label] = tenantSpecificData[label];
                         }
                     }
-                }).error(function (data) {
-                    console.log("Configuration file not found");
-                }).catch(function (e) {
-                    console.log("Configuration file not found");
-                });
+                    return result;
+                };
+
             };
-            checkAndSetData = function (scope, tenantdata) {
-                $http.get('scripts/config/default_UiConfig.json').success(function (defaultdata) {
-                    var result = {};
-                    result = diff(defaultdata, tenantdata);
-                    if (result.enableUIDisplayConfiguration != null && result.enableUIDisplayConfiguration == true) {
-                        scope.response = result;
-                        scope.responseDefaultGisData = result;
-                    }
-                }).error(function (tenantdata) {
-                    console.log("Configuration file not found");
-                }).catch(function (e) {
-                    console.log("Configuration file not found");
-                });
-            };
-
-            var diff = function (defaultdata, tenantdata) {
-                var result = defaultdata;
-                for (var lebel in tenantdata) {
-                    if (typeof tenantdata[lebel] == 'object' && typeof defaultdata[lebel] == 'object') {
-                        result[lebel] = diff(defaultdata[lebel], tenantdata[lebel]);
-
-                    } else {
-                        result[lebel] = tenantdata[lebel];
-                    }
-                }
-                return result;
-            };
-
-
         }
     });
 
-    mifosX.ng.services.service('UIConfigService', ['$q','$http',mifosX.services.UIConfigService]).run(function ($log) {
-        $log.info("UIConfigService initialized");
+ mifosX.ng.services.service('UIConfigService', ['$q','$http',mifosX.services.UIConfigService]).run(function ($log) {
+    $log.info("UIConfigService initialized");
 
-    });
+});
 
 }(mifosX.services || {}));
