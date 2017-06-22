91d69d5358199b7c9b2492b8baf656d13613639f
jyothsnag <jyothsna@confluxtechnologies.com>
2017-06-22 11:19:33 +0530

    #FINFLUX-797 working days Advanced UI config

--- app/scripts/controllers/organization/EditWorkingDaysController.js
+++ app/scripts/controllers/organization/EditWorkingDaysController.js
@@ -3,6 +3,12 @@
         EditWorkingDaysController: function (scope, routeParams, resourceFactory, location, dateFilter, $filter) {
             scope.workingDays = [];
             scope.selectedRepaymentType = "";
+            scope.advancedRescheduleDetail = [];
+            scope.deletedRescheduleDetail = [];
+            scope.fromWeekDay = "";
+            scope.advancedRepaymentRescheduleType = "";
+            scope.toWeekDay = "";
+            scope.isShowAdvancedSettingForWorkingDays=true;
             scope.compareWith = [
                 {name: "MO", value: "Monday", code: "day.monday"},
                 {name: "TU", value: "Tuesday", code: "day.tuesday"},
@@ -29,6 +35,7 @@
                         scope.workingDays.push({
                             day : scope.compareWith[i].value,
                             code : scope.compareWith[i].code,
+                            name : scope.compareWith[i].name,
                             value : true
                         });
                     }
@@ -36,13 +43,16 @@
                         scope.workingDays.push({
                             day : scope.compareWith[i].value,
                             code : scope.compareWith[i].code,
+                            name : scope.compareWith[i].name,
                             value : false
                         });
                     }
                 }
+                scope.advancedRescheduleDetail = data.advancedRescheduleDetail;
             });
             resourceFactory.workingDaysResourceTemplate.get(function(data){
                 scope.repaymentRescheduleOptions = data.repaymentRescheduleOptions;
+                scope.repaymentRescheduleOptionsForAdvancedReschedule = data.repaymentRescheduleOptionsForAdvancedReschedule;
             });
 
             scope.showLabel = function(day){
@@ -64,6 +74,30 @@
                         selectedDays = selectedDays.concat(scope.compareWith[i].name);
                     }
                 }
+                this.formData.advancedRescheduleDetail = [];
+                for(var i in scope.advancedRescheduleDetail){
+                    var advancedDetail = scope.advancedRescheduleDetail[i];
+                    var detail = {};
+                    if(advancedDetail.id){
+                        detail.id = advancedDetail.id;
+                    }
+                    detail.fromWeekDay = advancedDetail.fromWeekDay;
+                     // repaymentRescheduleType=6 :Move to Next Working Week Day
+                     // repaymentRescheduleType=7 :Move to Previous Working Week Day
+                    if(advancedDetail.repaymentReschedulingType){
+                        detail.repaymentRescheduleType = advancedDetail.repaymentReschedulingType.id;
+                        if(detail.repaymentRescheduleType == 6 || detail.repaymentRescheduleType == 7) {
+                            detail.toWeekDay = advancedDetail.toWeekDay;
+                        }
+                    }
+                    this.formData.advancedRescheduleDetail.push(detail);
+                }
+
+                for(var i in scope.deletedRescheduleDetail){
+                    var deleteDetail = scope.deletedRescheduleDetail[i];
+                    this.formData.advancedRescheduleDetail.push(deleteDetail);   
+                }
+
                 if(selectedDays == ""){
                     selectedDays = ",";
                 }
@@ -74,9 +108,38 @@
                 resourceFactory.workingDaysResource.put(this.formData, function(data){
                     location.path('/organization/');
                 })
+            }
+            scope.addAdvancedDetail = function(){
 
+                var detail = {};
+                detail.fromWeekDay = scope.fromWeekDay;
+                detail.toWeekDay = scope.toWeekDay;
+                detail.repaymentReschedulingType = scope.advancedRepaymentRescheduleType;
+                scope.advancedRescheduleDetail.push(detail);
+
+                scope.fromWeekDay = "";
+                scope.advancedRepaymentRescheduleType = "";
+                scope.toWeekDay = "";
+            }
+            scope.deleteDetail = function(index){
+                var detail = scope.advancedRescheduleDetail[index];
+                if(detail.id){
+                    var deletedDetail = {};
+                    deletedDetail.id = detail.id;
+                    deletedDetail.delete = true;    
+                    scope.deletedRescheduleDetail.push(deletedDetail);
+                }
+                scope.advancedRescheduleDetail.splice(index, 1);
             }
 
+            scope.showOrHideAdvancedSettings = function () {
+
+                if (scope.isShowAdvancedSettingForWorkingDays) {
+                    scope.isShowAdvancedSettingForWorkingDays = false;
+                } else{
+                    scope.isShowAdvancedSettingForWorkingDays = true;
+                }
+            };
         }
     });
     mifosX.ng.application.controller('EditWorkingDaysController', ['$scope', '$routeParams', 'ResourceFactory', '$location', 'dateFilter', '$filter',  mifosX.controllers.EditWorkingDaysController]).run(function ($log) {
