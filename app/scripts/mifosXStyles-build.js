aed0e359dced8500f7e61f9e7860d97f459b5c4e
Nazeer Hussain Shaik <nazeer.shaik@confluxtechnologies.com>
2017-07-11 17:44:22 +0530

    FINFLUX-41 Client Specific Fonts

--- app/scripts/mifosXStyles-build.js
+++ app/scripts/mifosXStyles-build.js
@@ -1,4 +1,29 @@
 define(['underscore'], function () {
+    var tenantIdentifier  ;
+    var tenantConfig = {
+        "ncc" : "laowebfont"
+    } ;
+    var mainLink = document.createElement("a");
+    mainLink.href = window.location.href;
+    if (mainLink.hostname.indexOf('confluxcloud.com') >= 0) {
+        var domains = hostname.split('.');
+        if (domains[0] == "demo") {
+            tenantIdentifier = 'default' ;
+        } else {
+            tenantIdentifier = domains[0] ;
+        }
+    }else {
+        if (window.location.search) {
+            var params = window.location.search.slice(1).split("&");
+            for (var i = 0; i < params.length; i++) {
+                var tmp = params[i].split("=");
+                if(tmp[0] == "tenantIdentifier") {
+                    tenantIdentifier = unescape(tmp[1]);
+                }
+            }
+        }
+    }
+
     var styles = {
         css: [
             'bootstrap.min.css',
@@ -12,6 +37,9 @@ define(['underscore'], function () {
             'loading-bar.css'
         ]
     };
+    if(tenantConfig[tenantIdentifier]) {
+        styles.css.splice(styles.css.length, 0, tenantConfig[tenantIdentifier]) ;
+    }
 
     require(_.reduce(_.keys(styles), function (list, pluginName) {
         return list.concat(_.map(styles[pluginName], function (stylename) {
