a7397b8b15c6b52ac5eab29b7c9a122d34d453d4
Sajan <sajan@confluxtechnologies.com>
2017-08-11 13:48:46 +0530

    FINFLUX-1603 : Income And Expense Summary

--- app/scripts/controllers/task/activity/cashflowActivityController.js
+++ app/scripts/controllers/task/activity/cashflowActivityController.js
@@ -43,7 +43,7 @@
                 angular.forEach(scope.incomeAndExpenses, function(data){
                     if(!_.isUndefined(data.incomeExpenseData.cashFlowCategoryData.categoryEnum) && data.incomeExpenseData.cashFlowCategoryData.categoryEnum.id == 1){
                         if(!_.isUndefined(data.totalIncome) && !_.isNull(data.totalIncome)){
-                            total = total + data.totalIncome;
+                            total = total + data.totalIncome-data.totalExpense;
                         }
                     }
                 });
@@ -55,7 +55,7 @@
                 angular.forEach(scope.incomeAndExpenses, function(data){
                     if(!_.isUndefined(data.incomeExpenseData.cashFlowCategoryData.categoryEnum) && data.incomeExpenseData.cashFlowCategoryData.categoryEnum.id == 2){
                         if(!_.isUndefined(data.totalIncome) && !_.isNull(data.totalIncome)){
-                            total = total + data.totalIncome;
+                            total = total + data.totalIncome-data.totalExpense;
                         }
                     }
                 });
