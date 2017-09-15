a53afe16e1f03dfcb10c419fc41ca6b72eb89a20
Madhukar <madhukar@confluxtechnologies.com>
2017-09-15 16:50:45 +0530

    FINFLUX-2166: Add user create activity

---
+++ app/scripts/controllers/task/activity/UserActivityController.js
@@ -0,0 +1,24 @@
+(function (module) {
+    mifosX.controllers = _.extend(module, {
+        UserActivityController: function (scope, resourceFactory, location, route) {
+            scope.users = [];
+            scope.isCreateuser = false;
+            resourceFactory.userListResource.getAllUsers(function (data) {
+                scope.users = data;
+            });
+            scope.showCreateUser = function(){
+            	scope.isCreateuser=true
+            };
+
+            scope.$on('submitPerformed', function (event, data) {
+                route.reload();
+            });
+            scope.$on('cancelPerformed', function (event, data) {
+                scope.isCreateuser = false;
+            });
+        }
+    });
+    mifosX.ng.application.controller('UserActivityController', ['$scope', 'ResourceFactory', '$location', '$route', mifosX.controllers.UserActivityController]).run(function ($log) {
+        $log.info("UserActivityController initialized");
+    });
+}(mifosX.controllers || {}));
