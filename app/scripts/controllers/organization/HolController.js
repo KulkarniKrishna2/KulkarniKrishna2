b3a5a23e954b18d3d271f714f1cab07d41d1946d
Sangamesh Belleri <sangamesh.belleri@confluxtechnologies.com>
2017-10-31 18:15:36 +0530

    FINFLUX-2651: Holiday issue 2-sorting changes

--- app/scripts/controllers/organization/HolController.js
+++ app/scripts/controllers/organization/HolController.js
@@ -4,6 +4,8 @@
             scope.holidays = [];
             scope.offices = [];
             scope.formData = {};
+            scope.showDeletedRecords = false;
+            scope.showPastRecords = false;
 
             scope.routeTo = function (id) {
                 location.path('/viewholiday/' + id);
@@ -21,7 +23,7 @@
                 scope.saveSC();
             };
 
-            resourceFactory.holResource.getAllHols({officeId: scope.formData.officeId}, function (data) {
+            resourceFactory.holResource.getAllHols({officeId: scope.formData.officeId, pastRecords:scope.showPastRecords, deletedRecords : scope.showDeletedRecords}, function (data) {
                 scope.holidays = data;
             });
 
@@ -29,12 +31,32 @@
                 scope.offices = data;
             });
 
-            scope.getHolidays = function () {
+            scope.getHolidays = function (pastrecords, deletedRecords) {
                 scope.searchCriteria.holidays[1] = scope.formData.officeId;
                 scope.saveSC();
-                resourceFactory.holResource.getAllHols({officeId: scope.formData.officeId}, function (data) {
+                resourceFactory.holResource.getAllHols({officeId: scope.formData.officeId, pastRecords:pastrecords, deletedRecords : deletedRecords}, function (data) {
                     scope.holidays = data;
                 });
+            };
+
+            scope.getPastHolidaysData = function(){
+                if(scope.showPastRecords){
+                    scope.showPastRecords = false;
+                }else{
+                    scope.showPastRecords = true;
+                }
+                scope.showDeletedRecords = false;
+                scope.getHolidays(scope.showPastRecords, scope.showDeletedRecords);
+            };
+
+            scope.getDeletedHolidaysData = function(){
+                if(scope.showDeletedRecords){
+                    scope.showDeletedRecords = false;
+                }else{
+                    scope.showDeletedRecords = true;
+                }
+                scope.showPastRecords = false;
+                scope.getHolidays(scope.showPastRecords, scope.showDeletedRecords);
             };
         }
     });
