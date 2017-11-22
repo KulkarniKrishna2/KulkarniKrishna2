05ffc7c12859de0e3e605283b562764a708d2797
satish-conflux <satish.sajjan@confluxtechnologies.com>
2017-11-22 14:51:29 +0530

    FINFLUX-3232 : PDC - excel download option for list of PDC's in bulk screen

--- app/scripts/controllers/pdc/ManagePDCController.js
+++ app/scripts/controllers/pdc/ManagePDCController.js
@@ -3,7 +3,7 @@
  */
 (function (module) {
     mifosX.controllers = _.extend(module, {
-        ManagePDCController: function (scope, routeParams, resourceFactory, location, dateFilter, $modal) {
+        ManagePDCController: function (scope, routeParams, resourceFactory, location, dateFilter, $modal, excelExportTableService, $timeout) {
             scope.isSearchPDCData = false;
             scope.formData = {};
             resourceFactory.pdcSearchTemplateResource.getSearchTemplate({}, function (data) {
@@ -36,10 +36,11 @@
             scope.cancel = "cancel";
             scope.return = "return";
             scope.undo = "undo";
-            
+
             scope.back = function () {
                 delete scope.errorDetails;
                 scope.isSearchPDCData = false;
+                scope.isSelectAllPDC = false;
             };
 
             scope.isEnableThisAction = function (action) {
@@ -83,7 +84,7 @@
                         scope: scope,
                         resolve: {}
                     });
-                }else{
+                } else {
                     scope.errorDetails = [];
                     var errorObj = new Object();
                     errorObj.args = {
@@ -94,19 +95,43 @@
                 }
             };
 
-            scope.printDiv = function(print) {
+            scope.exportToPdf = function (print) {
                 var printContents = document.getElementById(print).innerHTML;
                 var popupWin = window.open('', '_blank', 'width=300,height=300');
                 popupWin.document.open();
                 popupWin.document.write('<html><head><link rel="stylesheet" type="text/css" href="styles/repaymentscheduleprintstyle.css" />' +
                     '</head><body onload="window.print()">' + printContents + '<br></body></html>');
                 popupWin.document.close();
-            }
+            };
 
+            scope.exportToExcel = function (tableId) { // ex: '#my-table'
+                var exportHref = excelExportTableService.tableToExcel(tableId, 'PDC Report');
+                $timeout(function () {
+                    window.location.href = exportHref;
+                }, 100); // trigger download
+            };
+
+            scope.isSelectAllPDC = false;
+            scope.checkOrUncheckAllSelectedPDC = function () {
+                scope.isSelectAllPDC = !scope.isSelectAllPDC;
+                if(scope.isSelectAllPDC){
+                    for(var i in scope.searchPDCData){
+                        if(scope.searchPDCData[i].chequeDate && scope.searchPDCData[i].chequeAmount){
+                            scope.searchPDCData[i].isChecked = true;
+                        }
+                    }
+                }else{
+                    for(var i in scope.searchPDCData){
+                        if(scope.searchPDCData[i].isChecked){
+                            scope.searchPDCData[i].isChecked = false;
+                        }
+                    }
+                }
+            };
         }
     });
 
-    mifosX.ng.application.controller('ManagePDCController', ['$scope', '$routeParams', 'ResourceFactory', '$location', 'dateFilter', '$modal', mifosX.controllers.ManagePDCController]).run(function ($log) {
+    mifosX.ng.application.controller('ManagePDCController', ['$scope', '$routeParams', 'ResourceFactory', '$location', 'dateFilter', '$modal', 'ExcelExportTableService', '$timeout', mifosX.controllers.ManagePDCController]).run(function ($log) {
         $log.info("ManagePDCController initialized");
     });
 }(mifosX.controllers || {}));
