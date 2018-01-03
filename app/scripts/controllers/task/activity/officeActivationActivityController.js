a35441618056c116128a31f9db049a6af38b5d76
himadri <himadri.mitra@confluxtechnologies.com>
2018-01-04 00:47:03 +0530

    FINFLUX-3246:User should not be allowed to complete the activity for Group Activation without activating the group

--- app/scripts/controllers/task/activity/officeActivationActivityController.js
+++ app/scripts/controllers/task/activity/officeActivationActivityController.js
@@ -4,15 +4,36 @@
             angular.extend(this, $controller('defaultActivityController', {$scope: scope}));
             scope.officeId = scope.taskconfig['officeId'];
             scope.formData = {};
+            scope.isOfficeActive = false;
 
             resourceFactory.officeResource.get({officeId: scope.officeId}, function(data){
                 scope.office = data;
+                if(data.status.id == 300){
+                    scope.isOfficeActive = true; 
+                }
             });
 
             scope.submit = function () {
                 resourceFactory.officeResource.save({ officeId: scope.officeId, command: 'activate' }, scope.formData, function (data) {
+                    if(data.changes.status.id == 300){
+                       scope.isOfficeActive = true; 
+                    }
                     location.path('/offices');
                 });
+            };
+            /*overriding doPreTaskActionStep method of defaultActivityController*/
+            scope.doPreTaskActionStep = function (actionName) {
+                if (actionName === 'activitycomplete') {
+                    if (!scope.isOfficeActive) {
+                        scope.setTaskActionExecutionError("error.message.office.activation.activity.cannot.complete.before.office.activation");
+                        return;
+                    } else {
+                        scope.doActionAndRefresh(actionName);
+                    }
+                }
+                else{
+                    scope.doActionAndRefresh(actionName);
+                }
             };
         }
 
