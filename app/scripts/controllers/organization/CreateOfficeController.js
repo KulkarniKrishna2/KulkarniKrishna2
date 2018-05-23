1169604ab3d4cbcf13d1b8d3cf1d0e40e804f244
saumya06292017 <saumya@confluxtechnologies.com>
2018-05-23 15:02:03 +0530

    FINFLUX-6161: Auto Office activation

--- app/scripts/controllers/organization/CreateOfficeController.js
+++ app/scripts/controllers/organization/CreateOfficeController.js
@@ -5,6 +5,7 @@
             scope.first = {};
             scope.first.date = new Date();
             scope.restrictDate = new Date();
+            scope.isSelfActive = scope.response.uiDisplayConfigurations.createOffice.selfActive;
             resourceFactory.officeResource.getAllOffices(function (data) {
                 scope.offices = data;
                 scope.formData = {
@@ -18,9 +19,22 @@
                 this.formData.dateFormat = scope.df;
                 this.formData.openingDate = reqDate;
                 resourceFactory.officeResource.save(this.formData, function (data) {
-                    location.path('/viewoffice/' + data.resourceId);
+                    if(scope.isSelfActive && data.resourceId){
+                        activateoffice(data.resourceId);
+                    }else{
+                        location.path('/viewoffice/' + data.resourceId); 
+                    }
                 });
             };
+
+            var activateoffice = function(officeId) {
+                resourceFactory.officeResource.save({ officeId: officeId, command: 'activate' }, {}, function (data) {
+                    if(data.changes.status.id == 300){
+                       scope.isOfficeActive = true;
+                       location.path('/viewoffice/' + data.resourceId); 
+                    }
+                });
+            }
         }
     });
     mifosX.ng.application.controller('CreateOfficeController', ['$scope', 'ResourceFactory', '$location', 'dateFilter', mifosX.controllers.CreateOfficeController]).run(function ($log) {
