33d6aa79385f969355d38fa68009be2a1645f625
ShruthiRajaram <shruthi@confluxtechnologies.com>
2018-05-31 13:06:48 +0530

    FINFLUX-6405 and FINFLUX-6406

--- app/scripts/controllers/task/activity/CreditBureauCheckActivityController.js
+++ app/scripts/controllers/task/activity/CreditBureauCheckActivityController.js
@@ -3,6 +3,7 @@
         CreditBureauCheckActivityController: function ($controller, scope, routeParams, $modal, resourceFactory, location, dateFilter, ngXml2json, route, $http, $rootScope, $sce, CommonUtilService, $route, $upload, API_VERSION) {
             angular.extend(this, $controller('defaultActivityController', { $scope: scope }));
 
+            scope.showBulkCBInitiate = false;
             function initTask() {
                 scope.centerId = scope.taskconfig.centerId;
                 resourceFactory.centerWorkflowResource.get({ centerId: scope.centerId, associations: 'groupMembers,loanaccounts,cbexistingloanssummary,clientcbcriteria,loanproposalreview' }, function (data) {
@@ -11,6 +12,14 @@
                     scope.clientClosureReasons = data.clientClosureReasons;
                     scope.groupClosureReasons = data.groupClosureReasons;
                     scope.officeId = scope.centerDetails.officeId;
+                    for(var i in scope.centerDetails.subGroupMembers){
+                        for(var j in scope.centerDetails.subGroupMembers[i].memberData){
+                            if(!scope.centerDetails.subGroupMembers[i].memberData[j].cbExistingLoansSummaryData && scope.centerDetails.subGroupMembers[i].memberData[j].loanAccountBasicData){
+                                scope.showBulkCBInitiate = true;
+                                break;
+                            }
+                        }
+                    }
                 });
 
             };
@@ -42,6 +51,22 @@
                             
                         })
                     
+                });
+            };
+
+            scope.initiateBulkCreditBureauReport = function () {    
+
+                scope.entityType = "center";
+                scope.isForce = true;
+                scope.isClientCBCriteriaToRun = true;
+
+                resourceFactory.creditBureauBulkReportResource.get({
+                    entityType: scope.entityType,
+                    entityId:  scope.centerId ,
+                    isForce: scope.isForce,
+                    isClientCBCriteriaToRun : scope.isClientCBCriteriaToRun
+                }, function (loansSummary) {
+                    initTask();
                 });
             };
 
