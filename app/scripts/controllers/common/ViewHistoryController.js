86435bf212a92860c981501d1a3369f07d987c5f
himadri <himadri.mitra@confluxtechnologies.com>
2018-06-07 16:42:28 +0530

    FINFLUX-6488:system should display the entity history at loan level.

--- app/scripts/controllers/common/ViewHistoryController.js
+++ app/scripts/controllers/common/ViewHistoryController.js
@@ -6,7 +6,20 @@
                 scope.historydatas = data;
 
             });
+            scope.back = function(){
+                if(routeParams.entityType == 'client'){
+                    location.path('/viewclient/' +routeParams.entityId);
+                }else if(routeParams.entityType == 'group'){
+                    location.path('/viewgroup/' +routeParams.entityId);
+                }else if(routeParams.entityType == 'office'){
+                    location.path('/viewoffice/' +routeParams.entityId);
+                }else if(routeParams.entityType == 'loan'){
+                    location.path('/viewloanaccount/' +routeParams.entityId);
+                }else if(routeParams.entityType == 'center'){
+                    location.path('/viewcenter/' +routeParams.entityId);
+                }
 
+            }
     }
     });
     mifosX.ng.application.controller('ViewHistoryController', ['$scope', 'ResourceFactory', '$location','$routeParams','dateFilter', '$route', mifosX.controllers.ViewHistoryController]).run(function ($log) {
