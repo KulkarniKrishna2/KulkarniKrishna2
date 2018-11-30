a246e4ce4a43712fa513f908a0013fe32df7d2d3
himadri <himadri.mitra@confluxtechnologies.com>
2018-11-30 12:27:00 +0530

    PB-2663:Change API For BC Transaction Mapping

--- app/scripts/controllers/accounting/CreateNewTransactionTypeController.js
+++ app/scripts/controllers/accounting/CreateNewTransactionTypeController.js
@@ -4,6 +4,8 @@
             scope.formData = {
                 locale: scope.optlang.code
             };
+            scope.showAcounting = false;
+            scope.transactionTypeMappings = [];
             scope.transactionTypeOptions = [];
             scope.productTypeOptions = [];
             resourceFactory.transactionTypeMappingTemplateResource.get(function (data) {
@@ -12,6 +14,8 @@
                 scope.loanProducts = data.loanProducts;
                 scope.glAccounts = data.glAccounts;
                 scope.savingsProducts = data.savingsProducts;
+                scope.accountLevelTypeOptions = data.accountLevelTypeOptions;
+                scope.transactionMappingOptions = data.transactionMappingOptions;
 
             });
             scope.changeProductType = function (productTypeId) {
@@ -22,7 +26,31 @@
                     scope.productOptions = scope.savingsProducts;
                 }
             }
+
+            scope.addMapping = function(){
+                scope.showAcounting = true;
+                scope.transactionTypeMappings.push({
+                   mappingTypeId: scope.transactionMappingOptions.length > 0 ? scope.transactionMappingOptions[0].id : '',
+                   creditGlId: scope.glAccounts.length > 0 ? scope.glAccounts[0].id : '',
+                   debitGlId: scope.glAccounts.length > 0 ? scope.glAccounts[0].id : ''
+                });
+            }
+            scope.deleteMaping = function(index){
+                scope.transactionTypeMappings.splice(index,1);
+                if(scope.transactionTypeMappings.length == 0){
+                   scope.showAcounting = false; 
+                }
+            }
             scope.submit = function () {
+                scope.formData.transactionMappings = [];
+                for(var i in scope.transactionTypeMappings){
+                    temp = {
+                       mappingTypeId: scope.transactionTypeMappings[0].mappingTypeId,
+                       creditGlId: scope.transactionTypeMappings[0].creditGlId,
+                       debitGlId: scope.transactionTypeMappings[0].debitGlId
+                    }
+                    scope.formData.transactionMappings.push(temp);
+                }
                 resourceFactory.transactionTypeMappingResource.save(scope.formData, function (data) {
                     location.path('/transactiontypemapping');
                 });
