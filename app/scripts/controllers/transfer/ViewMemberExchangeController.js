740621e19b9aae861d5d9a1885c9cf4bd39e7554
shilpa15 <shilpa@confluxtechnologies.com>
2018-12-12 17:39:52 +0530

    PB-2476 : Reject/approve confirm popup window for transfer entity should be displayed.

--- app/scripts/controllers/transfer/ViewMemberExchangeController.js
+++ app/scripts/controllers/transfer/ViewMemberExchangeController.js
@@ -1,6 +1,6 @@
 (function (module) {
     mifosX.controllers = _.extend(module, {
-        ViewMemberExchangeController: function (scope, resourceFactory, location, dateFilter, routeParams) {
+        ViewMemberExchangeController: function (scope, resourceFactory, location, dateFilter, routeParams, $modal) {
             scope.init = function () {
                 resourceFactory.bulkTransferResource.getAll({ transferType: 400 }, function (data) {
                     for (var i in data) {
@@ -15,15 +15,38 @@
                 });
             };
             scope.init();
-            scope.submit = function (id, action) {
-                resourceFactory.bulkTransferResource.actions({ branchTransferId: id, command: action }, {}, function (data) {
-                    scope.init();
+            scope.submit = function (id,action) {
+                $modal.open({
+                    templateUrl: 'memberExchangeAction.html',
+                    controller: MemberExchangeActionCtrl,
+                    resolve: {
+                        rejectParameterInfo: function () {
+                            return { 'bulkTransferId': id ,'action': action };
+                        }
+                    }
                 });
-            }
-
+            };
+            var MemberExchangeActionCtrl = function ($scope, $modalInstance,rejectParameterInfo) {
+                $scope.bulkTransferId = rejectParameterInfo.bulkTransferId;
+                $scope.action = rejectParameterInfo.action;
+                $scope.isApproveAction = false;
+            
+                if($scope.action == "approve"){
+                    $scope.isApproveAction = true;
+                }
+                $scope.confirm = function () {
+                    resourceFactory.bulkTransferResource.actions({ branchTransferId: $scope.bulkTransferId, command: $scope.action }, {}, function (data) {
+                        $modalInstance.close();
+                        scope.init();
+                    });
+                };
+                $scope.cancel = function () {
+                    $modalInstance.dismiss('cancel');
+                };
+            };
         }
     });
-    mifosX.ng.application.controller('ViewMemberExchangeController', ['$scope', 'ResourceFactory', '$location', 'dateFilter', '$routeParams', mifosX.controllers.ViewMemberExchangeController]).run(function ($log) {
+    mifosX.ng.application.controller('ViewMemberExchangeController', ['$scope', 'ResourceFactory', '$location', 'dateFilter', '$routeParams', '$modal', mifosX.controllers.ViewMemberExchangeController]).run(function ($log) {
         $log.info("ViewMemberExchangeController initialized");
     });
 }(mifosX.controllers || {}));
