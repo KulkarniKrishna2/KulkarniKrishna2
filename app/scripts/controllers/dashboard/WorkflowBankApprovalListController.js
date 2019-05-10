5ec57cbc145600be0a1cf0673f952957b40ed8ea
vamshivksp <vamshikrishna@confluxtechnologies.com>
2019-05-10 15:49:50 +0530

    PB-4653:Kotak approval page exception refresh issue

--- app/scripts/controllers/dashboard/WorkflowBankApprovalListController.js
+++ app/scripts/controllers/dashboard/WorkflowBankApprovalListController.js
@@ -306,6 +306,7 @@
             scope.changeInTab = function(grouping){
                 scope.filterBy = 'Invalid';
                 scope.tabGrouping = grouping;
+                delete scope.errorDetails;
                 scope.formData.crnSelectedOption = 'All CRN Status';
                 scope.checkForOption();
                 scope.formData.actionListSelectedOption = 'No Action Selected';
