af3bc30dea57c5ca33296cf9839d7f4208f5ef7e
shilpa15 <shilpa@confluxtechnologies.com>
2019-07-03 19:07:19 +0530

    FE-659 : Pagination has to be added in the Manage Code section.

--- app/scripts/controllers/system/CodeController.js
+++ app/scripts/controllers/system/CodeController.js
@@ -2,7 +2,9 @@
     mifosX.controllers = _.extend(module, {
         CodeController: function (scope, resourceFactory, location) {
             scope.codes = [];
-
+            scope.requestoffset=0;
+            scope.limit = 15;
+            
             scope.routeTo = function (id) {
                 location.path('/viewcode/' + id);
             }
@@ -18,9 +20,33 @@
                 scope.saveSC();
             };
 
-            resourceFactory.codeResources.getAllCodes(function (data) {
-                scope.codes = data;
-            });
+            scope.init = function(){
+                resourceFactory.codeResource.getAll({
+                    offset: scope.requestoffset,
+                    limit: scope.limit,
+                }, function(data){
+                    scope.codes = data;
+                    
+                });
+            }
+            scope.init();
+
+            scope.previousCodesRequest= function(){
+                if(scope.requestoffset != 0){
+                    scope.requestoffset = scope.requestoffset - scope.limit;
+                    if(scope.requestoffset <= 0){
+                        scope.requestoffset = 0;
+                    }
+                    scope.init();
+                }
+            } 
+
+            scope.nextCodesRequest= function(){
+                if(scope.codes.length == scope.limit){
+                    scope.requestoffset = scope.requestoffset + scope.limit;
+                    scope.init();
+                }
+            } 
         }
     });
     mifosX.ng.application.controller('CodeController', ['$scope', 'ResourceFactory', '$location', mifosX.controllers.CodeController]).run(function ($log) {
