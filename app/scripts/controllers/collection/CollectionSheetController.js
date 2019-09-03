1636010291fc6ed6b61919ccef8b457628907034
greeshmandrai <greeshma@confluxtechnologies.com>
2019-09-03 11:53:42 +0530

    FE-1185: On paying the whole amount till today, on collection sheet EMI is still displaying

--- app/scripts/controllers/collection/CollectionSheetController.js
+++ app/scripts/controllers/collection/CollectionSheetController.js
@@ -927,7 +927,7 @@
                 _.each(data.groups, function (group) {
                     _.each(group.clients,function(client){
                         _.each(client.loans,function(loan){
-                            if(!_.isUndefined(loan.installmentAmount) && !loan.lastPayment){
+                            if(!_.isUndefined(loan.installmentAmount) && !loan.lastPayment && loan.totalDue > 0){
                                 loan.totalDue = loan.installmentAmount;
                             }
                         });
