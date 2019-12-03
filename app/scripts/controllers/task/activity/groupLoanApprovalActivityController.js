519aee3037b75916ff69632c2b00979bfac9ca87
greeshmandrai <greeshma@confluxtechnologies.com>
2019-12-03 18:38:29 +0530

    FE-1856: Charges Getting Deleted at the loan application level

--- app/scripts/controllers/task/activity/groupLoanApprovalActivityController.js
+++ app/scripts/controllers/task/activity/groupLoanApprovalActivityController.js
@@ -71,11 +71,11 @@
                     scope.formData = applicationData;
                     scope.loanProductChange(applicationData.loanProductId);
                     resourceFactory.loanApplicationReferencesResource.getChargesByLoanAppId({
-                    loanApplicationReferenceId: scope.loanApplicationReferenceId,command: 'loanapplicationcharges'}, function(loanAppChargeData) {
+                    loanApplicationReferenceId: scope.loanApplicationReferenceId,command: 'loanapplicationchargeswithchargetemplate'}, function(loanAppChargeData) {
                         scope.loanAppChargeData = loanAppChargeData;
                         for (var i = 0; i < scope.loanAppChargeData.length; i++) {
                             if (scope.loanAppChargeData[i].chargeId) {
-                                scope.constructExistingCharges(i, scope.loanAppChargeData[i].chargeId);
+                                scope.constructExistingCharges(scope.loanAppChargeData[i].templateChargeData);
                             } else {
                                 currentIndex++;
                             }
@@ -85,29 +85,27 @@
                 currentIndex = 0;
             };
 
-            scope.constructExistingCharges = function(index, chargeId) {
-                resourceFactory.chargeResource.get({chargeId: chargeId,template: 'true'}, function(data) {
-                    data.chargeId = data.id;
-                    scope.charges.push(data);
-                    currentIndex++;
-                    if (scope.loanAppChargeData && currentIndex == scope.loanAppChargeData.length) {
-                        for (var i = 0; i < scope.charges.length; i++) {
-                            for (var j = 0; j < scope.loanAppChargeData.length; j++) {
-                                if (scope.charges[i].chargeId == scope.loanAppChargeData[j].chargeId) {
-                                    scope.charges[i].loanAppChargeId = scope.loanAppChargeData[j].loanAppChargeId;
-                                    scope.charges[i].loanApplicationReferenceId = scope.loanAppChargeData[j].loanApplicationReferenceId;
-                                    if (scope.loanAppChargeData[j].dueDate) {
-                                        scope.charges[i].dueDate = dateFilter(new Date(scope.loanAppChargeData[j].dueDate), scope.df);
-                                    }
-                                    scope.charges[i].amount = scope.loanAppChargeData[j].amount;
-                                    scope.charges[i].isMandatory = scope.loanAppChargeData[j].isMandatory;
-                                    scope.charges[i].isAmountNonEditable = scope.loanAppChargeData[j].isAmountNonEditable;
+            scope.constructExistingCharges = function(data) {
+                data.chargeId = data.id;
+                scope.charges.push(data);
+                currentIndex++;
+                if (scope.loanAppChargeData && currentIndex == scope.loanAppChargeData.length) {
+                    for (var i = 0; i < scope.charges.length; i++) {
+                        for (var j = 0; j < scope.loanAppChargeData.length; j++) {
+                            if (scope.charges[i].chargeId == scope.loanAppChargeData[j].chargeId) {
+                                scope.charges[i].loanAppChargeId = scope.loanAppChargeData[j].loanAppChargeId;
+                                scope.charges[i].loanApplicationReferenceId = scope.loanAppChargeData[j].loanApplicationReferenceId;
+                                if (scope.loanAppChargeData[j].dueDate) {
+                                    scope.charges[i].dueDate = dateFilter(new Date(scope.loanAppChargeData[j].dueDate), scope.df);
                                 }
+                                scope.charges[i].amount = scope.loanAppChargeData[j].amount;
+                                scope.charges[i].isMandatory = scope.loanAppChargeData[j].isMandatory;
+                                scope.charges[i].isAmountNonEditable = scope.loanAppChargeData[j].isAmountNonEditable;
                             }
                         }
-                        angular.copy(scope.charges, scope.existingCharges);
                     }
-                });
+                    angular.copy(scope.charges, scope.existingCharges);
+                }
             };
 
             scope.loanProductChange = function(loanProductId) {
