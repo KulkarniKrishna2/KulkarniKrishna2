806be5b1965e6418c17f3af74f476c204ae010dd
Sagar Desai <79748479+SagarFinFlux@users.noreply.github.com>
2021-08-06 17:07:43 +0530

    DFL-293-1:Loan Application Scorecard Visualization (Same as client) (#5315)

--- app/scripts/controllers/task/activity/loanapplicationscorecardActivityController.js
+++ app/scripts/controllers/task/activity/loanapplicationscorecardActivityController.js
@@ -1,65 +1,48 @@
 (function (module) {
     mifosX.controllers = _.extend(module, {
-        ApplicationScoreCardController: function (scope, routeParams, resourceFactory, location, $q, $modal) {
+        loanpplicationscorecardActivityController: function ($controller, scope, resourceFactory, API_VERSION, location, http, routeParams, API_VERSION, $upload, $rootScope, commonUtilService) {
+            angular.extend(this, $controller('defaultActivityController', {$scope: scope}));
 
-            scope.loanApplicationReferenceId = routeParams.loanApplicationReferenceId;
-            scope.formData = {};
-
-            resourceFactory.loanApplicationReferencesResource.getByLoanAppId({loanApplicationReferenceId: scope.loanApplicationReferenceId}, function (applicationData) {
-                scope.applicationData = applicationData;
-                scope.formData.clientId = applicationData.clientId;
-                scope.formData.groupId = applicationData.groupId;
-                if(scope.applicationData.expectedDisbursalPaymentType){
-                    scope.formData.expectedDisbursalPaymentType = scope.applicationData.expectedDisbursalPaymentType.id;
+            function checkTaskStatus() {
+                if(scope.isTaskCompleted()) {
+                    scope.loanApplicationScoreCardInitBlock = false;
+                    fetchScores();
                 }
-                if(scope.applicationData.expectedRepaymentPaymentType){
-                    scope.formData.expectedRepaymentPaymentType = scope.applicationData.expectedRepaymentPaymentType.id;
-                }
-                scope.loanProductChange(applicationData.loanProductId, false);
-            });
+            }
+            checkTaskStatus();
 
-            scope.loanProductChange = function (loanProductId, isNewCall) {
-                scope.inparams = {resourceType: 'template', activeOnly: 'true'};
-                if (scope.formData.clientId && scope.formData.groupId) {
-                    scope.inparams.templateType = 'jlg';
-                } else if (scope.formData.groupId) {
-                    scope.inparams.templateType = 'group';
-                } else if (scope.formData.clientId) {
-                    scope.inparams.templateType = 'individual';
-                }
-                if (scope.formData.clientId) {
-                    scope.inparams.clientId = scope.formData.clientId;
-                }
-                if (scope.formData.groupId) {
-                    scope.inparams.groupId = scope.formData.groupId;
-                }
-                scope.inparams.staffInSelectedOfficeOnly = true;
+            scope.loanApplicationScoreCardInitBlock = true;
+            scope.loanApplicationScoreCardInitNotice = false;
+            scope.loanApplicationScoreCardInitError = false;
+            scope.loanApplicationScoreCardInit = function() {
+                fetchScores();
+            }
 
-
-                scope.inparams.productId = loanProductId;
-
-                resourceFactory.loanResource.get(scope.inparams, function (data) {
-                    scope.loanaccountinfo = data;
-                    if (data.clientName) {
-                        scope.clientName = data.clientName;
-                    }
-                    if (data.group) {
-                        scope.groupName = data.group.name;
-                    }
-                });
-            };
-
-
+            scope.loanApplicationReferenceId = routeParams.loanApplicationId;
+            
             // For Fetching the score card details
             function fetchScores(){
                 resourceFactory.loanAppScoreCardResourceList.getAll({loanAppId:scope.loanApplicationReferenceId}, function (response) {
+                    scope.loanApplicationScoreCardInitError = false;
+                    scope.loanApplicationScoreCardInitNotice = false;
                     scope.showScore = true;
                     scope.scoreList = response;
                     console.log(response);
+                    if(scope.scoreList.length > 0) {
+                        scope.loanApplicationScoreCardInitBlock = false;
+                    } else {
+                        scope.loanApplicationScoreCardInitNotice = true;
+                    }
+
+                    if(scope.isTaskCompleted()) {
+                        scope.fetchScorecardDetail(scope.scoreList[0].name);
+                    }
+                },function(error) {
+                    scope.loanApplicationScoreCardInitError = true;
                 });
             }
 
-            fetchScores();
+            // fetchScores();
 
             scope.fetchScorecardDetail = function (selctedScoreKey) {
                 console.log(selctedScoreKey);
@@ -72,6 +55,7 @@
             };
 
             // Score Card Functions
+
             scope.inputObj;
             scope.outputErrorB = false;
             scope.outputExpand = false;
@@ -179,11 +163,9 @@
                     scope.ruleResultExpandValue = '';
                 }
             }
-
-
         }
     });
-    mifosX.ng.application.controller('ApplicationScoreCardController', ['$scope', '$routeParams', 'ResourceFactory', '$location', '$q', '$modal', mifosX.controllers.ApplicationScoreCardController]).run(function ($log) {
-        $log.info("ApplicationScoreCardController initialized");
+    mifosX.ng.application.controller('loanpplicationscorecardActivityController', ['$controller','$scope', 'ResourceFactory', 'API_VERSION', '$location', '$http', '$routeParams', 'API_VERSION', '$upload', '$rootScope', 'CommonUtilService', mifosX.controllers.loanpplicationscorecardActivityController]).run(function ($log) {
+        $log.info("loanpplicationscorecardActivityController initialized");
     });
 }(mifosX.controllers || {}));
